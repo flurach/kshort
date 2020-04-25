52f6e3670a0dfea8792a181ef556195cb2dfe9a1
Flurach <flurach.dev@gmail.com>
2020-04-25 13:14:24 +0300

    Switched to nedb

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-// dependencies
 const bodyParser = require('body-parser')
-const express    = require('express')
-const mongodb    = require('mongodb')
+const express = require('express')
+const app = express()
 
 
-// library intances
-const MongoClient = mongodb.MongoClient
-const app         = express()
+// plugins
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('static'))
 
 
-// config
-const APP_PORT = 3000
-
-const DB_HOST  = 'localhost'
-const DB_PORT  = '27017'
-const DB_NAME  = 'kshort'
-
-
-// connect to database
-function connect_database(callback) {
-    const url     = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
-    const options = { useUnifiedTopology: true }
-
-    MongoClient.connect(url, options, (error, db) => {
-        if (error)
-            throw error
-
-        callback(db.db(DB_NAME))
-        db.close()
-    })
-}
-
-
-// app config (pls don't touch)
-app
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(express.static('static'))
+// database
+const { AsyncNedb } = require('nedb-async')
+const aliases = new AsyncNedb({
+	filename: '.data/aliases.db',
+	autoload: true
+})
 
 
 // index page
@@ -44,58 +22,48 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/static/index.html'))
 
 
 // create alias route
-app.post('/', (req, res) => {
-    let { url, alias } = req.body
+app.post('/', async (req, res) => {
+	let { url, alias } = req.body
+	
+	// check fields
+	if (!url || !alias)
+		return res.send({ error: 'Fields are invalid' })
+	
+	// verify url
+	const invalid_character_found_in_url =
+		url.indexOf('<') != -1 || url.indexOf('>') != -1
+	if (invalid_character_found_in_url)
+		return res.send({ error: 'Invalid character in url' })
+	
+	// verify alias
+	const invalid_character_found_in_alias =
+		alias.indexOf('<') != -1 || alias.indexOf('>') != -1
+	if (invalid_character_found_in_alias)
+		return res.send({ error: 'Invalid character in alias' })
 
-    // check fields
-    if (url == undefined || alias == undefined)
-        return res.send({ status: 'error', message: 'Fields are invalid' })
+	// check if alias exists
+	const alias_exists = await aliases.asyncFindOne({ alias })
+	if (alias_exists)
+		return res.send({ error: `Alias '${alias}' exists` })
 
-    // verify url & alias
-    const invalid_character_found_in_url = url.indexOf('<') != -1 || url.indexOf('>') != -1
-    if (invalid_character_found_in_url)
-        return res.send({ status: 'error', message: 'Invalid character in url' })
-
-    const invalid_character_found_in_alias = alias.indexOf('<') != -1 || alias.indexOf('>') != -1
-    if (invalid_character_found_in_alias)
-        return res.send({ status: 'error', message: 'Invalid character in alias' })
-
-    // check if alias exists in db
-    connect_database(db => {
-        db.collection('urls').findOne({ alias }, (err, data) => {
-            if (err) throw err
-
-            if (data != undefined)
-                return res.send({ status: 'error', message: `Alias '${alias}' exists` })
-
-            // else, create alias
-            connect_database(db => db.collection('urls').insertOne({ url, alias }))
-            res.send({ status: 'success', alias })
-        })
-    })
+	// create alias
+	aliases.asyncInsert({ url, alias })
+	res.send({})
 })
 
 
 // redirect aliases
-app.get('/:alias', (req, res) => {
-    connect_database(db => {
+app.get('/:alias', async (req, res) => {
 
-        // check if alias exists
-        db.collection('urls').findOne({ alias: req.params.alias }, (err, data) => {
-            if (err) throw err
+	// check if alias exists
+	const alias = await aliases.asyncFindOne({ alias })
+	if (!alias)
+		return res.send('Url not found :(')
 
-            // if alias does not exist
-            if (data == undefined)
-                return res.send('Url does not exist')
-
-            res.redirect(data.url)
-        })
-
-    })
+	res.redirect(alias.url)
 })
 
 
 // app listen
-app.listen(APP_PORT, () =>
-    console.log(`App listening on port ${APP_PORT}`)
-)
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- static/js/script.js
+++ static/js/script.js
@@ -4,28 +4,25 @@ const alias = document.getElementById('alias')
 const result = document.getElementById('result')
 const submit = document.getElementById('submit')
 
-form.onsubmit = async e => {
-    e.preventDefault()
-
-    const data = {
-        url: url.value,
-        alias: alias.value
-    }
-
-    $.post('/', data, function(response) {
-        if (response.status == 'success') {
-            result.classList.remove('error')
-            result.innerHTML = document.location.href + response.alias
-            result.style.display = 'block'
-
-            url.style.display = 'none'
-            alias.style.display = 'none'
-            submit.style.display = 'none'
-        }
-        else {
-            result.classList.add('error')
-            result.innerHTML = response.message
-            result.style.display = 'block'
-        }
-    })
+form.onsubmit = e => {
+	e.preventDefault()
+	
+	const data = {
+		url: url.value,
+		alias: alias.value
+	}
+	
+	$.post('/', data, response => {
+		if (response.error)
+		return alert(response.error)
+		
+		// hide everything
+		url.style.display = 'none'
+		alias.style.display = 'none'
+		submit.style.display = 'none'
+		
+		// display result
+		result.innerHTML = document.location.href + alias.value
+		result.style.display = 'block'
+	})
 }
